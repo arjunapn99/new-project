8f4531f914b344dc7adde281d6336f9bcb12e4be
Arjun sahu <160587595+arjunapn99@users.noreply.github.com>
2024-06-07 09:08:08 +0000

    datatype.js

---
+++ .devcontainer/first_file/datatype.js
@@ -0,0 +1,10 @@
+console.log(123);
+
+console.log("arjun");
+
+let name="arjun";
+let age=25;
+let bool=true;
+let l= undifined ;
+
+console.log(typeof l)
---
+++ .devcontainer/first_file/variable.js
@@ -0,0 +1,12 @@
+const accountId = 108875516
+let accountEmail = "arjun@gmail.com"
+var accountPassward = "12345678"
+ accountCity = "satna"
+
+//  accountEmail="deepak@gmail.com"
+//  accountPassward="12345687"
+//  accountCity="rewa"
+
+// console.log(accountId);
+
+ console.table([accountId,accountEmail,accountPassward,accountCity]);
