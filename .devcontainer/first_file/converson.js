7b4ee8a968d331bb64c7c2f64581321e0a5ba39c
Arjun sahu <160587595+arjunapn99@users.noreply.github.com>
2024-06-08 14:10:48 +0000

    converson

--- .devcontainer/first_file/converson.js
+++ .devcontainer/first_file/converson.js
@@ -8,11 +8,24 @@
 // console.log(valueInNumber)
 
 
-let isLoggedIn=""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn=""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn)
 
-let program=99
-let rupee= String(program)
-console.log(rupee)
-console.log(typeof rupee)
+// let program=33
+// let rupee= String(program)
+// console.log(rupee)
+// console.log(typeof rupee)
+
+// let str1=5
+// let str2=69
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2+9)
+// console.log("1"+2+2+2)
+// console.log(1+"2"+5)
+// console.log(1+3+"5")
+
+console.log(+true);
+console.log(+"");
