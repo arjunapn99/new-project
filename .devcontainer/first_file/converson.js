ad4d9d37ad13e7d2e461c2560dffa1b21041b9b7
Arjun sahu <160587595+arjunapn99@users.noreply.github.com>
2024-06-09 18:31:31 +0000

    converson

--- .devcontainer/first_file/converson.js
+++ .devcontainer/first_file/converson.js
@@ -27,5 +27,17 @@
 // console.log(1+"2"+5)
 // console.log(1+3+"5")
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
+
+
+// console.log("2">1);
+// console.log(02<1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
