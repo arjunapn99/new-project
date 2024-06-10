2eedfa067ff47f3574bf006868ef50136d736218
Arjun sahu <160587595+arjunapn99@users.noreply.github.com>
2024-06-10 14:54:32 +0000

    summery.js

---
+++ .devcontainer/first_file/summery.js
@@ -0,0 +1,19 @@
+//     premtive type
+
+
+// data type =7= string ,number ,boolean,null, undifined,bigint ,symbol
+
+const score = 150 ;
+const number= 100.56
+const isLoggedIn=true
+const emaillId = null
+const nameId = undefined
+const id= Symbol('100')
+const anotherId= Symbol('150')
+
+console.log(id==anotherId)
+
+
+// reference type (non primtive )
+
+//data type = array , object , function
